42340b0c0aeadbe1f3b4d9894f9881c9ef21d579
Tanveer Ahmed <tanveerchandio432@gmail.com>
2026-09-03 22:09:48 +0500

    New exercise or arrays solved

--- 01_arrays/unique_values.cpp
+++ 01_arrays/unique_values.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
-int main(){
-        int arr[] = {1, 1, 2, 2, 3};
-        int size = sizeof(arr)/sizeof(int);
-        for (int i = 0; i < size; i++){
-            int unique = arr[i];
-            for(int j = i + 1; j < size; j++){
-                if(arr[j] != unique){
-                    cout << arr[j] << endl;
-                }
+
+int main() {
+    int arr[] = {1, 1, 2, 4, 2, 3};
+    int size = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < size; i++) {
+        bool found = false;
+        for (int j = 0; j < size; j++) {
+            if (i != j && arr[i] == arr[j]) {
+                found = true;
+                break;
             }
         }
-        
+        if (!found) {
+            cout << arr[i] << " ";
+        }
+    }
 }
